0236935702a67298021f78b6388bb7b437bc8fd3
Snezhana <snowwe25@rambler.ru>
2018-10-28 18:14:51 +0300

    style: formatting

--- test.js
+++ test.js
@@ -73,8 +73,12 @@ describe('Recursion', () => {
     it('1', () => {
         const tree = {
             "value": 100,
-            "left": {"value": 90, "left": {"value": 70}, "right": {"value": 99}},
-            "right": {"value": 120, "left": {"value": 110}, "right": {"value": 130}}
+            "left": {
+                "value": 90, "left": {"value": 70}, "right": {"value": 99}
+            },
+            "right": {
+                "value": 120, "left": {"value": 110}, "right": {"value": 130}
+            }
         };
         const array = recursion(tree);
         assert.equal(array, [[100], [90, 120], [70, 99, 110, 130]]);
